7ead7184691f25ef159ef1435e46f6046af66d71
Bri Turner <blturner3527@gmail.com>
2020-01-14 05:01:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,5 @@
 def make_sandwich(element1, element2)
   yield("A #{element1} and #{element2} sandwich")
 end
+
+make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b} on wheat" }

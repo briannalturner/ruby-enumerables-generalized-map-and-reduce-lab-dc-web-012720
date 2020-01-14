d4ea33464e4f7256d4a52c8c4ba3da0271179e44
Bri Turner <blturner3527@gmail.com>
2020-01-14 05:02:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,4 +2,4 @@ def make_sandwich(element1, element2)
   yield("A #{element1} and #{element2} sandwich")
 end
 
-puts make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
+puts make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b} on wheat" }

2240c4bf0b833b26c80f951c851f19283eb44b57
Bri Turner <blturner3527@gmail.com>
2020-01-14 05:22:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ def map(source_array)
   new_arr
 end
 
-def reduce(source_array, starting_value = nil)
+def reduce(source_array, starting_value=nil)
   if starting_value
     total = starting_value
     index = 0 

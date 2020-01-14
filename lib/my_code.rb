1d2418848abdd404da37131bce53bac438c1ff8d
Bri Turner <blturner3527@gmail.com>
2020-01-14 05:07:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,3 +3,16 @@ def make_sandwich(element1, element2)
 end
 
 puts make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b} on wheat" }
+
+def map(source_array)
+  new_arr = []
+  index = 0
+  
+  while index < source_array.length do
+    
+    index += 1
+  end
+  new_arr
+end
+
+map(source_array) { |c| 

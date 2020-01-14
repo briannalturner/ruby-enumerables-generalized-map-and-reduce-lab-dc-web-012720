bd1b494bb3de6ff7575546b03d061b68090b6448
Bri Turner <blturner3527@gmail.com>
2020-01-14 04:59:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,8 +1,10 @@
 def make_sandwich(element1, element2)
   base = "A #{element1} and #{element2}"
-  yield(base)
+  puts base
+  yield
+  base
 end
  
-puts make_sandwich("gator", "gumbo") do |innards|
-  "#{innards} on rye"
+puts make_sandwich("chicken", "a sense of malaise") do |innards|
+  puts "making some tasty stuff..."
 end
